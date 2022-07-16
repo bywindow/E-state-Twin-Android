9aeddb7d70abb7bff7de347a01cac1b6a3534bd8
dionidip <paulus0617@gmail.com>
2022-07-16 23:54:36 +0900

    [SFJ-95] 🚑️ fix: connect fragment to bottom navigation

--- app/src/main/java/com/idiot/e_state_twin_android/core/ui/MainActivity.kt
+++ app/src/main/java/com/idiot/e_state_twin_android/core/ui/MainActivity.kt
@@ -3,20 +3,32 @@ package com.idiot.e_state_twin_android.core.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.idiot.e_state_twin_android.R
+import com.idiot.e_state_twin_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
-        setContentView(R.layout.activity_main)
+        initBinding()
+        initNavigation()
     }
 
-//    fun setBottomNavigationBackground() {
-//        val radius = resources.getDimension(R.dimen.bottom_nav_corner)
-//        val bottomNavigation = findViewById<>(R.id.bottom_navigation)
-//        val bottomNavigationViewBackground = bottomNavigation.background
-//    }
+    private fun initBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+    }
+
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
 }
