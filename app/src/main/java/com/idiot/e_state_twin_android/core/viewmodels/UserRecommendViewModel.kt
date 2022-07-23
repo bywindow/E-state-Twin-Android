df75c8a39e84baf0b753d6e680c70f4aca81f44e
dionidip <paulus0617@gmail.com>
2022-07-23 19:41:31 +0900

    Add: recommend house item layout in main page

---
+++ app/src/main/java/com/idiot/e_state_twin_android/core/viewmodels/UserRecommendViewModel.kt
@@ -0,0 +1,7 @@
+package com.idiot.e_state_twin_android.core.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class UserRecommendViewModel : ViewModel() {
+
+}
--- app/src/main/java/com/idiot/e_state_twin_android/features/modelViewer/GltfViewerFragment.kt
+++
@@ -1,4 +0,0 @@
-package com.idiot.e_state_twin_android.features.modelViewer
-
-class GltfViewerFragment {
-}
