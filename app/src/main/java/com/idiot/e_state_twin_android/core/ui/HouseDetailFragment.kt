1b808b7eeb4f272e80e765843167aebb15d930b3
dionidip <paulus0617@gmail.com>
2022-08-20 20:06:22 +0900

    Refactor: Modularize

--- app/src/main/java/com/idiot/e_state_twin_android/core/ui/HouseDetailFragment.kt
+++ app/src/main/java/com/idiot/e_state_twin_android/core/ui/HouseDetailFragment.kt
@@ -1,16 +1,11 @@
 package com.idiot.e_state_twin_android.core.ui
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.idiot.e_state_twin_android.R
 import com.idiot.e_state_twin_android.core.adapters.HouseImageSliderAdapter
