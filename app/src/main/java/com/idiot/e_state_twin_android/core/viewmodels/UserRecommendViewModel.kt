5cc52e501a2c615a1b245ae63200fcdbff86eb07
dionidip <paulus0617@gmail.com>
2022-08-31 19:32:41 +0900

    Add: common-ui module

--- app/src/main/java/com/idiot/e_state_twin_android/core/viewmodels/UserRecommendViewModel.kt
+++ app/src/main/java/com/idiot/e_state_twin_android/core/viewmodels/UserRecommendViewModel.kt
@@ -7,6 +7,6 @@ import com.idiot.e_state_twin_android.core.data.models.RecommendedHouse
 // need to fix
 class UserRecommendViewModel(item: RecommendedHouse) {
     val recommendedHouse: RecommendedHouse = item
-    var isShowModel : Boolean = true
+    var isShowModel : Boolean = false
     val imgResId = if (isShowModel) R.drawable.img_model_thumbnail else R.drawable.img_interior_dummy
 }
