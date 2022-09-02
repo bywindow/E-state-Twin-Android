1c048c5a09017ef717a02c76f28ffadee16a2ba4
dionidip <paulus0617@gmail.com>
2022-09-02 11:47:03 +0900

    Add: interest page toolbar

--- common-ui/src/main/java/com/idiot/common_ui/interest/InterestFragment.kt
+++ common-ui/src/main/java/com/idiot/common_ui/interest/InterestFragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.idiot.common_ui.R
+import com.idiot.common_ui.databinding.FragmentInterestBinding
 
 class InterestFragment : Fragment() {
+
+    private lateinit var binding: FragmentInterestBinding
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_interest, container, false)
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_interest, container,false
+        )
+        viewPager = binding.viewPager
+        tabLayout = binding.tabMenu
+
+        subscribeUi(binding)
+
+        return binding.root
+    }
+
+    private fun subscribeUi(binding: FragmentInterestBinding) {
+        binding.toolbarInterest.appBar.elevation = 0f
+        binding.toolbarInterest.appBar.stateListAnimator = null
     }
 }
