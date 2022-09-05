17dfc623b5adeefc8533a0a945579b292f6f7127
dionidip <paulus0617@gmail.com>
2022-09-05 23:47:11 +0900

    Add: picture recycler view

--- common-ui/src/main/java/com/idiot/common_ui/register/adapters/RegisterPagerAdapter.kt
+++ common-ui/src/main/java/com/idiot/common_ui/register/adapters/RegisterPagerAdapter.kt
@@ -3,6 +3,7 @@ package com.idiot.common_ui.register.adapters
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.idiot.common_ui.register.NewRegisterRequestFragment
+import com.idiot.common_ui.register.RegisterForBrokerFragment
 
 const val NEW_REGISTER_PAGE_INDEX = 0
 const val PAUSE_REGISTER_PAGE_INDEX = 1
@@ -11,7 +12,7 @@ class RegisterPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) 
 
     private val tabFragmentsCreator: Map<Int, () -> Fragment> = mapOf(
         NEW_REGISTER_PAGE_INDEX to { NewRegisterRequestFragment() },
-        PAUSE_REGISTER_PAGE_INDEX to { NewRegisterRequestFragment() }
+        PAUSE_REGISTER_PAGE_INDEX to { RegisterForBrokerFragment() }
     )
     override fun getItemCount(): Int {
         return tabFragmentsCreator.size
