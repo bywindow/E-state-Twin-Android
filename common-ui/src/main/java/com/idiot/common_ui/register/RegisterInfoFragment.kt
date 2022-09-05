f7d4c159e3afb7a763c1cc3ef5e8b79ff791fe86
dionidip <paulus0617@gmail.com>
2022-09-05 22:34:30 +0900

    Add: navigation back button in toolbar

--- common-ui/src/main/java/com/idiot/common_ui/register/RegisterInfoFragment.kt
+++ common-ui/src/main/java/com/idiot/common_ui/register/RegisterInfoFragment.kt
@@ -6,19 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.google.android.material.appbar.MaterialToolbar
 import com.idiot.common_ui.R
 import com.idiot.common_ui.databinding.FragmentRegisterInfoBinding
 
 class RegisterInfoFragment : Fragment() {
-    
+
     private lateinit var binding: FragmentRegisterInfoBinding
+    private lateinit var toolbar: MaterialToolbar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register_info, container, false)
+        toolbar = binding.toolbarRegisterInfo.toolbar
+
+        subscribeUi(binding)
+
         return binding.root
+    }
+
+    private fun subscribeUi(binding: FragmentRegisterInfoBinding) {
+        toolbar.setNavigationIcon(R.drawable.ic_navigate_up)
+        toolbar.setNavigationOnClickListener {
+            it.findNavController().navigateUp()
+        }
     }
 
 }
