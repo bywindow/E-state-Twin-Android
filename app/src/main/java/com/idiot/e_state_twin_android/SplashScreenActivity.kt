0701466a6bf8ec1726b639d890d0ed672c47d36d
dionidip <paulus0617@gmail.com>
2022-09-18 02:23:47 +0900

    change login button UI

--- app/src/main/java/com/idiot/e_state_twin_android/SplashScreenActivity.kt
+++ app/src/main/java/com/idiot/e_state_twin_android/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.idiot.common_ui.MainActivity
+import com.idiot.feature.login.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,8 @@ class SplashScreenActivity : AppCompatActivity() {
          *  token 이 없거나 만료되었다면 LoginActivity 로 이동
          *  token 이 있다면 유저정보를 가지고 MainActivity 로 이동
          */
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
+//        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
 }
--- feature/login/src/main/java/com/idiot/feature/login/LoginActivity.kt
+++ feature/login/src/main/java/com/idiot/feature/login/LoginActivity.kt
@@ -1,11 +1,51 @@
 package com.idiot.feature.login
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.common.SignInButton
+import com.idiot.feature.login.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        subscribeUi(binding)
+    }
+
+    private fun subscribeUi(binding: ActivityLoginBinding) {
+        binding.googleButton.setSize(SignInButton.SIZE_ICON_ONLY)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+//            supportActionBar?.hide()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                val controller = window.insetsController
+                controller.let {
+                    it!!.hide(WindowInsets.Type.navigationBars())
+                    it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                }
+            } else {
+                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        // Hide the nav bar and status bar
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+            }
+        }
     }
 }
