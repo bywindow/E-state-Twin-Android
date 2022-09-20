764cf9deedbd6803d7b111956cdd6cff7579173c
dionidip <paulus0617@gmail.com>
2022-09-20 13:42:12 +0900

    Add: type selector spinner

--- common-ui/src/main/java/com/idiot/common_ui/register/RegisterHouseInfoFragment.kt
+++ common-ui/src/main/java/com/idiot/common_ui/register/RegisterHouseInfoFragment.kt
@@ -1,14 +1,19 @@
 package com.idiot.common_ui.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.idiot.common_ui.R
+import com.idiot.common_ui.databinding.FragmentRegisterHouseInfoBinding
+import com.idiot.common_ui.register.adapters.BuildingTypeSpinnerAdapter
 
 class RegisterHouseInfoFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentRegisterHouseInfoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +24,16 @@ class RegisterHouseInfoFragment : BottomSheetDialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_house_info, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_register_house_info, container, false
+        )
+        initBuildingTypeSpinner(binding)
+        return binding.root
+    }
+
+    private fun initBuildingTypeSpinner(binding: FragmentRegisterHouseInfoBinding) {
+        val adapter = BuildingTypeSpinnerAdapter(requireContext(), R.layout.list_item_building_type_spinner, resources.getStringArray(R.array.building_type_list))
+        binding.buildingTypeSelector.adapter = adapter
     }
 
 }
---
+++ common-ui/src/main/java/com/idiot/common_ui/register/adapters/BuildingTypeSpinnerAdapter.kt
@@ -0,0 +1,55 @@
+package com.idiot.common_ui.register.adapters
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.idiot.common_ui.R
+import com.idiot.common_ui.databinding.ListItemBuildingTypeSpinnerBinding
+import java.lang.Exception
+
+class BuildingTypeSpinnerAdapter(
+    context: Context,
+    @LayoutRes private val resId: Int,
+    private val types: Array<String>
+) : ArrayAdapter<String>(context, resId, types) {
+
+    override fun getCount() = types.size
+
+    override fun getItem(position: Int) = types[position]
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding : ListItemBuildingTypeSpinnerBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context), R.layout.list_item_building_type_spinner, parent, false)
+        val model = types[position]
+        try {
+            binding.type = model
+            binding.buildingTypeItem.setTextColor(ContextCompat.getColor(context,R.color.register_info_edit_text_hint))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return binding.root
+    }
+
+    override fun isEnabled(position: Int): Boolean {
+        return position != 0
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding : ListItemBuildingTypeSpinnerBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context), R.layout.list_item_building_type_spinner, parent, false)
+        val model = types[position]
+        try {
+            binding.type = model
+            if (position != 0) binding.buildingTypeItem.setTextColor(Color.BLACK)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return binding.root
+    }
+}
