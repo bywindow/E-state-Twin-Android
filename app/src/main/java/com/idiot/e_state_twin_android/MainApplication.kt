84a9acfea7b4a2ef2ebbb02d6ec1245ac1674631
dionidip <paulus0617@gmail.com>
2022-09-28 16:36:14 +0900

    refactor: Main application

--- app/src/main/java/com/idiot/e_state_twin_android/MainApplication.kt
+++ app/src/main/java/com/idiot/e_state_twin_android/MainApplication.kt
@@ -1,13 +1,7 @@
 package com.idiot.e_state_twin_android
 
 import android.app.Application
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application(), Configuration.Provider {
-
-  override fun getWorkManagerConfiguration(): Configuration =
-    Configuration.Builder().build()
-
-}
+class MainApplication : Application()
