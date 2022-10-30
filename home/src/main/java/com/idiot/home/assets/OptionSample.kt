dd7bf5c329171872e2429dfd3f0343649af3e15f
dionidip <paulus0617@gmail.com>
2022-10-31 00:18:08 +0900

    Add: asset category

--- home/src/main/java/com/idiot/home/assets/OptionSample.kt
+++ home/src/main/java/com/idiot/home/assets/OptionSample.kt
@@ -4,111 +4,126 @@ import com.idiot.data.repository.R
 import com.idiot.model.HouseOption
 
 fun optionSample(): List<HouseOption> {
-    return listOf(
-        HouseOption(
-            id = 10,
-            key = "ac",
-            icon = R.drawable.ic_option_ac,
-            desc = "에어컨",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 11,
-            key = "laundry",
-            icon = R.drawable.ic_option_laundry,
-            desc = "세탁기",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 12,
-            key = "refrigerator",
-            icon = R.drawable.ic_option_refrigerator,
-            desc = "냉장고",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 13,
-            key = "gas",
-            icon = R.drawable.ic_option_gas,
-            desc = "가스레인지",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 14,
-            key = "wave",
-            icon = R.drawable.ic_option_wave,
-            desc = "전자레인지",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 15,
-            key = "doorlock",
-            icon = R.drawable.ic_option_doorlock,
-            desc = "전자도어락",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 16,
-            key = "induction",
-            icon = R.drawable.ic_option_induction,
-            desc = "인덕션",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 17,
-            key = "tv",
-            icon = R.drawable.ic_option_tv,
-            desc = "TV",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 20,
-            key = "bed",
-            icon = R.drawable.ic_option_bed,
-            desc = "침대",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 21,
-            key = "closet",
-            icon = R.drawable.ic_option_closet,
-            desc = "옷장",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 22,
-            key = "desk",
-            icon = R.drawable.ic_option_desk,
-            desc = "책상",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 23,
-            key = "shoes",
-            icon = R.drawable.ic_option_shoes,
-            desc = "신발장",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 30,
-            key = "bidet",
-            icon = R.drawable.ic_option_bidet,
-            desc = "비데",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 40,
-            key = "wall",
-            icon = R.drawable.ic_option_wall,
-            desc = "벽지",
-            hasOption = false
-        ),
-        HouseOption(
-            id = 41,
-            key = "curtain",
-            icon = R.drawable.ic_option_curtain,
-            desc = "커튼",
-            hasOption = false
-        ),
-    )
+  return listOf(
+    HouseOption(
+      id = 10,
+      key = "ac",
+      category = "HOMEAPPLIANCES",
+      icon = R . drawable . ic_option_ac,
+      desc = "에어컨",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 11,
+      key = "laundry",
+      category = "HOMEAPPLIANCES",
+      icon = R.drawable.ic_option_laundry,
+      desc = "세탁기",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 12,
+      key = "refrigerator",
+      category = "HOMEAPPLIANCES",
+      icon = R.drawable.ic_option_refrigerator,
+      desc = "냉장고",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 13,
+      key = "gas",
+      category = "HOMEAPPLIANCES",
+      icon = R.drawable.ic_option_gas,
+      desc = "가스레인지",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 14,
+      key = "wave",
+      category = "HOMEAPPLIANCES",
+      icon = R.drawable.ic_option_wave,
+      desc = "전자레인지",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 15,
+      key = "doorlock",
+      category = "HOMEAPPLIANCES",
+      icon = R.drawable.ic_option_doorlock,
+      desc = "전자도어락",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 16,
+      key = "induction",
+      category = "HOMEAPPLIANCES",
+      icon = R.drawable.ic_option_induction,
+      desc = "인덕션",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 17,
+      key = "tv",
+      category = "HOMEAPPLIANCES",
+      icon = R.drawable.ic_option_tv,
+      desc = "TV",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 20,
+      key = "bed",
+      category = "FURNITURE",
+      icon = R.drawable.ic_option_bed,
+      desc = "침대",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 21,
+      key = "closet",
+      category = "FURNITURE",
+      icon = R.drawable.ic_option_closet,
+      desc = "옷장",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 22,
+      key = "desk",
+      category = "FURNITURE",
+      icon = R.drawable.ic_option_desk,
+      desc = "책상",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 23,
+      key = "shoes",
+      category = "FURNITURE",
+      icon = R.drawable.ic_option_shoes,
+      desc = "신발장",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 30,
+      key = "bidet",
+      category = "BATHROOM",
+      icon = R.drawable.ic_option_bidet,
+      desc = "비데",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 40,
+      key = "wall",
+      category = "INTERIOR",
+      icon = R.drawable.ic_option_wall,
+      desc = "벽지",
+      hasOption = false
+    ),
+    HouseOption(
+      id = 41,
+      key = "curtain",
+      category = "INTERIOR",
+      icon = R.drawable.ic_option_curtain,
+      desc = "커튼",
+      hasOption = false
+    ),
+  )
 }
--- model/src/main/java/com/idiot/model/HouseOption.kt
+++ model/src/main/java/com/idiot/model/HouseOption.kt
@@ -5,6 +5,7 @@ import java.io.Serializable
 data class HouseOption(
     val id: Int,
     val icon: Int,
+    val category: String,
     val key: String,
     val desc: String,
     var hasOption: Boolean,
