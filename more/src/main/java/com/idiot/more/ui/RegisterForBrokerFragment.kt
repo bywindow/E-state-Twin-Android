1a4c26a1b05dd17afb14c29fafddca94f1427d2a
dionidip <paulus0617@gmail.com>
2022-11-06 21:47:19 +0900

    Add: get posting estate api

--- more/src/main/java/com/idiot/more/ui/RegisterForBrokerFragment.kt
+++ more/src/main/java/com/idiot/more/ui/RegisterForBrokerFragment.kt
@@ -5,22 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.idiot.more.databinding.FragmentRegisterForBrokerBinding
+import com.idiot.more.ui.adapter.RegisterNewInquiryAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class RegisterForBrokerFragment : Fragment() {
 
-    private lateinit var binding: FragmentRegisterForBrokerBinding
+  private lateinit var binding: FragmentRegisterForBrokerBinding
+  private val viewModel: RegisterForBrokerViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    super.onCreateView(inflater, container, savedInstanceState)
 
-        binding = FragmentRegisterForBrokerBinding.inflate(inflater, container, false)
+    binding = FragmentRegisterForBrokerBinding.inflate(inflater, container, false)
+    binding.registerPostingList.adapter = RegisterNewInquiryAdapter(viewModel.postingEstate.value)
 
-        return binding.root
+    getEstateList()
+
+    return binding.root
+  }
+
+  private fun getEstateList() {
+    viewLifecycleOwner.lifecycleScope.launch {
+      viewModel.getPostingEstateList().collect {
+        binding.registerPostingList.adapter = RegisterNewInquiryAdapter(viewModel.postingEstate.value)
+      }
     }
+  }
 
 }
---
+++ more/src/main/java/com/idiot/more/ui/RegisterForBrokerViewModel.kt
@@ -0,0 +1,31 @@
+package com.idiot.more.ui
+
+import androidx.lifecycle.ViewModel
+import com.idiot.data.repository.BrokerEstateListRepository
+import com.idiot.data.repository.UserPreferenceRepository
+import com.idiot.model.EstateListBrokerResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterForBrokerViewModel @Inject constructor(
+  private val userPreferenceRepository: UserPreferenceRepository,
+  private val brokerEstateListRepository: BrokerEstateListRepository
+) : ViewModel() {
+  private val _postingEstate = MutableStateFlow<List<EstateListBrokerResponse>>(emptyList())
+  val postingEstate = _postingEstate.asStateFlow()
+
+  fun getPostingEstateList() = flow {
+    val token = userPreferenceRepository.getAccessToken().getOrNull().orEmpty()
+    val response = brokerEstateListRepository.requestGetEstateListBroker(token, "POST_DOING")
+    Timber.d("$response")
+    if (response != null) {
+      _postingEstate.value = response
+    }
+    emit(RegisterEvent.GetBrokerEstateList)
+  }
+}
